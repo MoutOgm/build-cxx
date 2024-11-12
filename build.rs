0a6fe6a4a42ffbcdf0ba5ed1eb8bbac5f6ac91df
MoutOgm <bouttier29.alex@orange.fr>
2024-11-12 14:52:43 +0000

    build with dynamic crate name

--- build.rs
+++ build.rs
@@ -1,13 +1,23 @@
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
+    let crate_name = env!("CARGO_PKG_NAME");
     let _build = cxx_build::bridge("src/lib.rs")
         .std("c++17")
-        .compile("libutils.a")
-        ;
-    fs::copy("../target/cxxbridge/utils/src/lib.rs.h", "../target/cxxbridge/utils.h").unwrap();
-    // fs::rename("../target/cxxbridge/utils/src/lib.rs.h", "../target/cxxbridge/utils.h").unwrap();
+        .compile(&format!("lib{}.a", crate_name));
+
+    // Get the root directory of the crate
+    let crate_root = env!("CARGO_MANIFEST_DIR");
+
+    // Define your source and destination paths relative to the crate root
+    let src = PathBuf::from(crate_root).join(format!("target/cxxbridge/{}/src/lib.rs.h", crate_name));
+    let dst = PathBuf::from(crate_root).join(format!("target/cxxbridge/{}.h", crate_name));
+
+    // Copy the file
+    fs::copy(&src, &dst).expect("Failed to copy file");
+
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rustc-env=TARGET=release");
-    println!("cargo::warning=Export C++ headers to cxxbridge/utils/src");
+    println!("cargo::warning=Export C++ headers to cxxbridge/{}/src", crate_name);
 }
