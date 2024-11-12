a353b6287b8847f5216878cf8b607f241873c0f8
MoutOgm <bouttier29.alex@orange.fr>
2024-11-12 14:59:19 +0000

    build dynamic crate name and remove errors

--- build.rs
+++ build.rs
@@ -4,20 +4,19 @@ use std::path::PathBuf;
 fn main() {
     let crate_name = env!("CARGO_PKG_NAME");
     let _build = cxx_build::bridge("src/lib.rs")
-        .std("c++17")
-        .compile(&format!("lib{}.a", crate_name));
+        .std("c++17");
 
     // Get the root directory of the crate
     let crate_root = env!("CARGO_MANIFEST_DIR");
 
     // Define your source and destination paths relative to the crate root
     let src = PathBuf::from(crate_root).join(format!("target/cxxbridge/{}/src/lib.rs.h", crate_name));
-    let dst = PathBuf::from(crate_root).join(format!("target/cxxbridge/{}.h", crate_name));
+    let dst = PathBuf::from(crate_root).join(format!("target/cxxbridge/{}.hpp", crate_name));
 
     // Copy the file
     fs::copy(&src, &dst).expect("Failed to copy file");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rustc-env=TARGET=release");
-    println!("cargo::warning=Export C++ headers to cxxbridge/{}/src", crate_name);
+    println!("cargo::warning=Export C++ headers");
 }
